2dd0393482915b1b1e3398eb55acb49565d9f111
Suuuuu <99704389+MrSudayon@users.noreply.github.com>
2022-10-29 01:29:32 +0800

    Update index.php

--- sms/index.php
+++ sms/index.php
@@ -1,14 +1,3 @@
-<!--
-	require $_SERVER['DOCUMENT_ROOT'].'../bossb/sms/vendor/autoload.php';
-	
-	$messagebird = new \MessageBird\Client('zDKqwLK3vsINavrtbAJHKMVTM');
-	$message = new \MessageBird\Objects\Message();
-	$message->originator ='BossB';
-	$message->recipients =array(+639058297643);
-	$message->body ='bossbburger', <br>,'please prepare COD 0.00 for delivery of your total order';
-	
-	$messagebird->messages->create($message);	
--->
 
 <?php
 	require $_SERVER['DOCUMENT_ROOT'].'../bossb/sms/vendor/autoload.php';
